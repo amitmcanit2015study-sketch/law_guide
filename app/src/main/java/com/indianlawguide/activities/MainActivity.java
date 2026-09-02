5ad0a98fc59fb5fcbb9d57e2c981b8905eb33b87
amitkatiyar <amit.katiyar@humanitics.com>
2026-09-03 00:26:13 +0530

    issue

--- app/src/main/java/com/indianlawguide/activities/MainActivity.java
+++ app/src/main/java/com/indianlawguide/activities/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.indianlawguide.utils.LocaleHelper.applyAppLanguage(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
